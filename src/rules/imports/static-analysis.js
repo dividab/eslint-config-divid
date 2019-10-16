94e82a78bc83825e80df4aaaa59f91c3075d85a3
Jonas Kello <jonas.kello@gmail.com>
2019-10-16 22:03:19 +0200

    Some import rules

---
+++ src/rules/imports/static-analysis.js
@@ -0,0 +1,19 @@
+module.exports = {
+  plugins: ["import"],
+  rules: {
+    "import/no-unresolved": "off", // Checked by Typescript
+    "import/named": "off", // Checked by Typescript
+    "import/default": "off", // Checked by Typescript
+    "import/namespace": "off", // Checked by Typescript
+    "import/no-restricted-paths": "off",
+    "import/no-absolute-path": "error",
+    "import/no-dynamic-require": "error",
+    "import/no-internal-modules": "off",
+    "import/no-webpack-loader-syntax": "error",
+    "import/no-self-import": "error",
+    "import/no-cycle": ["error", { maxDepth: Infinity }],
+    "import/no-useless-path-segments": "error",
+    "import/no-relative-parent-imports": "off",
+    "import/no-unused-modules": "off"
+  }
+};
