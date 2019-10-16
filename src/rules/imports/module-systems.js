2759637cbf6b406c3cf7fa9f60dc17ffd160d331
Jonas Kello <jonas.kello@gmail.com>
2019-10-16 22:12:30 +0200

    import

---
+++ src/rules/imports/module-systems.js
@@ -0,0 +1,9 @@
+module.exports = {
+  plugins: ["import"],
+  rules: {
+    "import/unambiguous": "off",
+    "import/no-commonjs": "error",
+    "import/no-amd": "error",
+    "import/no-nodejs-modules": "off"
+  }
+};
