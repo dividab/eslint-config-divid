4251609f19b7b9e49e491fd5bb231de381c3a7b5
Jonas Kello <jonas.kello@gmail.com>
2020-09-27 13:47:41 +0200

    Test index

--- tests/config.test.js
+++ tests/config.test.js
@@ -16,13 +16,22 @@ async function getErrors(configFile) {
   return await cli.lintText("", { filePath: "src/index.js" });
 }
 
-describe("Validate ESLint configs", () => {
-  ["src/rules/core/errors.js", "src/rules/typescript-eslint/all.js"].forEach(
-    (file) => {
-      it(`load config ${file} in ESLint to validate all rules are correct`, async () => {
-        const lintResults = await getErrors(file);
-        expect(lintResults[0].messages).toEqual([]);
-      });
-    }
-  );
+// describe("Validate ESLint configs", () => {
+//   ["src/rules/core/errors.js", "src/rules/typescript-eslint/all.js"].forEach(
+//     (file) => {
+//       it(`load config ${file} in ESLint to validate all rules are correct`, async () => {
+//         const lintResults = await getErrors(file);
+//         expect(lintResults[0].messages).toEqual([]);
+//       });
+//     }
+//   );
+// });
+
+describe("Validate ESLint config", () => {
+  ["src/index.js"].forEach((file) => {
+    it(`load config ${file} in ESLint to validate all rules are correct`, async () => {
+      const lintResults = await getErrors(file);
+      expect(lintResults[0].messages).toEqual([]);
+    });
+  });
 });
